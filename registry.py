ab57ac42a92d00a893699a33623c5b92db68658e
PO <57624309+Po-creator@users.noreply.github.com>
2019-11-24 11:22:17 +0300

    Update registry.py
    Am I right?

--- registry.py
+++ registry.py
@@ -3,5 +3,6 @@
 registry = [
     ("Hydrogen.txt", 0.0000899, ["H", "H2", "Hydrogen", "Водород", "Wasserstoff", "Hydrogène", "Idrogeno", "Hidrógeno"] ),
     ("Helium.txt"  , 0.0001753, ["He", "Helium", "Гелий", "Hélium", "Elio", "Helio"] ),
-    ("Berillium.txt"  , 1.848, ["Be", "Berillium", "Бериллий", "Béryllium", "Berilio", "Berillio"] )
+    ("Berillium.txt"  , 1.848, ["Be", "Berillium", "Бериллий", "Béryllium", "Berilio", "Berillio"] ),
+    ("Carbon.txt" , 2, ["C", "Carbon", "Углерод", "Kohlenstoff", "Carbono", "Carbonio"])
 ]
